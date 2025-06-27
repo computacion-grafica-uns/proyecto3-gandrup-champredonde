bf6a0304f616c5bf3da3c67211dafa222b0c578d
Laureano Gandrup <laureanogandrup01@gmail.com>
2025-06-27 16:35:30 -0300

    Agregamos Anomalias a Dormitorio (falta Intruso)

---
+++ Assets/Scripts/Anomalias/Dormitorio/AnomaliaImagenDorm.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnomaliaImagenDorm : AnomaliaImagen
+{
+    public Material materialToActivate, materialToRemove;
+    public override bool CheckAnomalyType(string type)
+    {
+        return type.Equals("Imagen");
+    }
+
+    public override void Activate()
+    {
+        objectToActivate.GetComponent<Renderer>().material = materialToActivate;
+        activated = true;
+    }
+
+    public override void Deactivate()
+    {
+        objectToActivate.GetComponent<Renderer>().material = materialToRemove;
+        activated = false;
+    }
+}
+
