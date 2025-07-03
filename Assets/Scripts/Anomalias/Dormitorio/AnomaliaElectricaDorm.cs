e72b3670591895d613f17959bd168c11b3619a5d
fernando champredonde <fernandochampredonde@gmail.com>
2025-07-03 20:05:58 -0300

    Arreglamos la anomalía eléctricadel baño

--- Assets/Scripts/Anomalias/Dormitorio/AnomaliaElectricaDorm.cs
+++ Assets/Scripts/Anomalias/Dormitorio/AnomaliaElectricaDorm.cs
@@ -4,34 +4,13 @@ using UnityEngine.Video;
 
 public class AnomaliaElectricaDorm : AnomaliaElectrica
 {
-    public GameObject pantallaApagadaGO;
-    public GameObject pantallaEncendidaGO;
-
     public AudioSource audioSource;
     public Light luzPantalla;
-    public VideoPlayer videoPlayer;
-
-    void Awake()
-    {
-        Deactivate(); // Asegura que arranque apagada
-    }
-
-    void Start()
-    {
-        if (videoPlayer != null)
-        {
-            videoPlayer.Prepare();
-        }
-    }
-
+    public Material materialPantallaPrendida, materialPantallaApagada;
+   
     public override void Activate()
     {
-        Debug.Log("🔌 [AnomaliaElectricaDorm] ACTIVADA en " + gameObject.name);
-        base.Activate();
-
-        // Alternar pantallas
-        if (pantallaApagadaGO != null) pantallaApagadaGO.SetActive(false);
-        if (pantallaEncendidaGO != null) pantallaEncendidaGO.SetActive(true);
+        objectToActivate.GetComponent<Renderer>().material = materialPantallaPrendida;
 
         // Activar sonido
         if (audioSource != null && !audioSource.isPlaying)
@@ -41,23 +20,13 @@ public class AnomaliaElectricaDorm : AnomaliaElectrica
         if (luzPantalla != null)
             luzPantalla.enabled = true;
 
-        // Reproducir video
-        if (videoPlayer != null && !videoPlayer.isPlaying)
-        {
-            videoPlayer.frame = 0;
-            videoPlayer.Play();
-        }
-
         activated = true;
     }
 
     public override void Deactivate()
     {
-        base.Deactivate();
-
         // Alternar pantallas
-        if (pantallaApagadaGO != null) pantallaApagadaGO.SetActive(true);
-        if (pantallaEncendidaGO != null) pantallaEncendidaGO.SetActive(false);
+        objectToActivate.GetComponent<Renderer>().material = materialPantallaApagada;
 
         // Detener sonido
         if (audioSource != null && audioSource.isPlaying)
@@ -67,15 +36,6 @@ public class AnomaliaElectricaDorm : AnomaliaElectrica
         if (luzPantalla != null)
             luzPantalla.enabled = false;
 
-        // Detener video
-        if (videoPlayer != null && videoPlayer.isPlaying)
-            videoPlayer.Stop();
-
         activated = false;
-    }
-
-    public override bool CheckAnomalyType(string type)
-    {
-        return type.Equals("Electrica");
     }
 }
